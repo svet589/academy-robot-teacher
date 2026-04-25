b6d1c5194c1ccc6df4939aaa27f48a3653daba2b
svet589 <d99428850@gmail.com>
2026-04-25 20:35:37 +0300

    world-facts.js

---
+++ assets/js/data/world-facts.js
@@ -0,0 +1,32 @@
+const WORLD_FACTS = {
+    planets: [
+        { name: 'Меркурий', order: 1, temp: '+430/-180°C', fact: 'Самая быстрая планета' },
+        { name: 'Венера', order: 2, temp: '+465°C', fact: 'Самая горячая' },
+        { name: 'Земля', order: 3, temp: '+15°C', fact: 'Наш дом!' },
+        { name: 'Марс', order: 4, temp: '-63°C', fact: 'Красная планета' },
+        { name: 'Юпитер', order: 5, temp: '-145°C', fact: 'Самая большая' },
+        { name: 'Сатурн', order: 6, temp: '-178°C', fact: 'Знаменит кольцами' },
+        { name: 'Уран', order: 7, temp: '-224°C', fact: 'Лежит на боку' },
+        { name: 'Нептун', order: 8, temp: '-218°C', fact: 'Самая ветреная' }
+    ],
+    bodyParts: [
+        { part: 'Сердце', function: 'Качает кровь', fact: '100 000 ударов в день' },
+        { part: 'Лёгкие', function: 'Дышат', fact: '20 000 вдохов в день' },
+        { part: 'Мозг', function: 'Думает', fact: '86 млрд нейронов' },
+        { part: 'Желудок', function: 'Переваривает', fact: 'Вмещает 4 литра' },
+        { part: 'Глаза', function: 'Видят', fact: '10 млн цветов' }
+    ],
+    animals: [
+        { name: 'Белый медведь', habitat: 'Арктика', food: 'Рыба', fact: 'Крупнейший хищник' },
+        { name: 'Жираф', habitat: 'Саванна', food: 'Листья', fact: 'Рост до 6 метров' },
+        { name: 'Пингвин', habitat: 'Антарктида', food: 'Рыба', fact: 'Не летает, плавает' },
+        { name: 'Кенгуру', habitat: 'Австралия', food: 'Трава', fact: 'Прыгает на 9 метров' },
+        { name: 'Слон', habitat: 'Африка', food: 'Трава', fact: 'Весит до 7 тонн' }
+    ],
+    cities: [
+        { city: 'Москва', country: 'Россия', population: '12 млн', fact: 'Основана в 1147 г.' },
+        { city: 'Лондон', country: 'Великобритания', population: '9 млн', fact: 'Столица Англии' },
+        { city: 'Париж', country: 'Франция', population: '2 млн', fact: 'Эйфелева башня' },
+        { city: 'Пекин', country: 'Китай', population: '21 млн', fact: 'Запретный город' }
+    ]
+};
